461278cbf050f6e2e2fae7a2a733f1418d96dc64
Jack <jiazhenz@umich.edu>
2022-10-01 12:41:21 -0400

    test new group

--- howtrader/api/websocket/websocket_client.py
+++ howtrader/api/websocket/websocket_client.py
@@ -31,7 +31,8 @@ class WebsocketClient:
         self._active: bool = False
         self._host: str = ""
 
-        self._session: ClientSession = ClientSession()
+        self._session: ClientSession = None
+        self._create_client_session()
         self._ws: ClientWebSocketResponse = None
         self._loop: AbstractEventLoop = None
 
@@ -41,6 +42,9 @@ class WebsocketClient:
 
         self._last_sent_text: str = ""
         self._last_received_text: str = ""
+
+    async def _create_client_session(self):
+        self._session = ClientSession()
 
     def init(
         self,
