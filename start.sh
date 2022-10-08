48ab7ae1eed3fea44eeec1315e72fc433377c4fe
Jack Zhao <jackzhao516@utexas.edu>
2022-10-08 17:13:51 -0400

    top 500 alert fixed; top 300 added

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-nohup python -u tv_script.py > nohup_log.out 2>&1 &
+rm -rf alert_100.log
+rm -rf alert_300.log
+rm -rf alert_500.log
+
+nohup python3 alert_system.py alert_100 > alert_100.log 2>&1 &
+nohup python3 alert_system.py alert_300 > alert_300.log 2>&1 &
+nohup python3 alert_system.py alert_500 > alert_500.log 2>&1 &
