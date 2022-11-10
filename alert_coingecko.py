f11d915e77599fb3c0d63012ff28a3d767d0c53d
Jack <jiazhenz@umich.edu>
2022-11-10 02:47:42 -0500

    recursive alert system

--- alert_coingecko.py
+++ alert_coingecko.py
@@ -60,50 +60,50 @@ class CoinGecKo100(CoinGecKo):
         self.spot_over_ma_4h = None
 
     def h12_init(self):
-        try:
-            price = self.cg.get_coin_market_chart_by_id(id=self.coin_id, vs_currency='usd', days=90)
-            price = price['prices']
-            if len(price) < 2150:
-                self.less_90_days = True
-            # price = [i[1] for i in price]
-            self.list_12h = np.zeros(180, dtype=np.float64)
-            price.reverse()
-            counter = 0
-            for i in range(0, len(price), 12):
-                self.list_12h[counter] = price[i][1]
-                counter += 1
-                # self.current_time_12h = price[i][0]
-            # self.current_time = price[-1][0]
-            self.counter_12h = len(price) // 12
-            self.ma_12h = np.sum(self.list_12h)/self.counter_12h
-            self.spot_over_ma_12h = price[0][1] > self.ma_12h
-            print(f"h12 init: {self.coin_id}, ma_12h: {self.ma_12h}, spot_over_ma_12h: {self.spot_over_ma_12h}")
-        except Exception as e:
-            sleep(60)
-            self.h12_init()
+        # try:
+        price = self.cg.get_coin_market_chart_by_id(id=self.coin_id, vs_currency='usd', days=90)
+        price = price['prices']
+        if len(price) < 2150:
+            self.less_90_days = True
+        # price = [i[1] for i in price]
+        self.list_12h = np.zeros(180, dtype=np.float64)
+        price.reverse()
+        counter = 0
+        for i in range(0, len(price), 12):
+            self.list_12h[counter] = price[i][1]
+            counter += 1
+            # self.current_time_12h = price[i][0]
+        # self.current_time = price[-1][0]
+        self.counter_12h = len(price) // 12
+        self.ma_12h = np.sum(self.list_12h)/self.counter_12h
+        self.spot_over_ma_12h = price[0][1] > self.ma_12h
+        print(f"h12 init: {self.coin_id}, ma_12h: {self.ma_12h}, spot_over_ma_12h: {self.spot_over_ma_12h}")
+        # except Exception as e:
+        #     sleep(60)
+        #     self.h12_init()
 
     def h4_init(self):
-        try:
-            price = self.cg.get_coin_market_chart_by_id(id=self.coin_id, vs_currency='usd', days=34)
-            price = price['prices']
-            if len(price) < 790:
-                self.less_34_days = True
+        # try:
+        price = self.cg.get_coin_market_chart_by_id(id=self.coin_id, vs_currency='usd', days=34)
+        price = price['prices']
+        if len(price) < 790:
+            self.less_34_days = True
 
-            self.list_4h = np.zeros(200, dtype=np.float64)
-            price.reverse()
-            counter = 0
-            for i in range(0, min(len(price), 800), 4):
-                self.list_4h[counter] = price[i][1]
-                counter += 1
+        self.list_4h = np.zeros(200, dtype=np.float64)
+        price.reverse()
+        counter = 0
+        for i in range(0, min(len(price), 800), 4):
+            self.list_4h[counter] = price[i][1]
+            counter += 1
 
-            self.counter_4h = min(len(price), 800) // 4
-            self.ma_4h = np.sum(self.list_4h)/self.counter_4h
-            self.spot_over_ma_4h = price[0][1] > self.ma_4h
-            print(f"h4 init: {self.coin_id}, ma_4h: {self.ma_4h}, spot_over_ma_4h: {self.spot_over_ma_4h}")
-        except Exception as e:
-            print("h4 init error")
-            sleep(60)
-            self.h4_init()
+        self.counter_4h = min(len(price), 800) // 4
+        self.ma_4h = np.sum(self.list_4h)/self.counter_4h
+        self.spot_over_ma_4h = price[0][1] > self.ma_4h
+        print(f"h4 init: {self.coin_id}, ma_4h: {self.ma_4h}, spot_over_ma_4h: {self.spot_over_ma_4h}")
+        # except Exception as e:
+        #     print("h4 init error")
+        #     sleep(60)
+        #     self.h4_init()
 
     def minute_update(self, update_ma_12=False, update_ma_4=False):
         price = self.cg.get_price(ids=self.coin_id, vs_currencies='usd', include_last_updated_at=True,
@@ -146,25 +146,25 @@ class CoinGecKo100(CoinGecKo):
         # print(f"{self.coin_id} spot: {str(price)} H4 ma200: {str(self.ma_4h)}")
 
     def alert_spot_100(self):
-        try:
-            self.h12_init()
-            self.h4_init()
-            last_update = None
-            minute_counter_12h = 1
-            minute_counter_4h = 1
-            while SETTINGS["100"]:
-                if last_update:
-                    last_update.join()
-                last_update = threading.Thread(target=self.minute_update, args=(minute_counter_12h % 720 == 0,
-                                                                                minute_counter_4h % 240 == 0))
-                last_update.start()
-                minute_counter_12h %= 720
-                minute_counter_12h += 1
-                minute_counter_4h %= 240
-                minute_counter_4h += 1
-                sleep(60)
-        except Exception as e:
-            pass
+        # try:
+        self.h12_init()
+        self.h4_init()
+        last_update = None
+        minute_counter_12h = 1
+        minute_counter_4h = 1
+        while SETTINGS["100"]:
+            if last_update:
+                last_update.join()
+            last_update = threading.Thread(target=self.minute_update, args=(minute_counter_12h % 720 == 0,
+                                                                            minute_counter_4h % 240 == 0))
+            last_update.start()
+            minute_counter_12h %= 720
+            minute_counter_12h += 1
+            minute_counter_4h %= 240
+            minute_counter_4h += 1
+            sleep(60)
+        # except Exception as e:
+        #     pass
 
 
 def alert_100_function(coin_id, symbol, prod=True):
