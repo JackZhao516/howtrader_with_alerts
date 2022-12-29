eae00e2a9fa02b39e1dc8d14db8453a20794103e
Jack <jiazhenz@umich.edu>
2022-12-28 22:40:09 -0500

    5min 10* bar alert

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ while true;
 do
     DATE=`date | cut -d' ' -f4`
 
-    if [[ $DATE == "03:37:00" ]]
+    if [[ $DATE == "03:42:00" ]]
     then
         conda activate mytrader
         rm -rf alert_5min_0.log
