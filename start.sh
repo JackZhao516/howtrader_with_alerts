4db9d5b76b3c69a10a3f4cfc07e0e73563df9d6b
Jack <jiazhenz@umich.edu>
2023-01-10 13:42:19 -0500

    price/volume alert

--- start.sh
+++ start.sh
@@ -2,18 +2,15 @@
 
 while true;
 do
-    DATE=`date | cut -d' ' -f5`
+    DATE=`date | cut -d' ' -f4`
+    DATE1=`date | cut -d' ' -f5`
 
-    if [[ $DATE == "02:57:00" ]]
+    if [[ $DATE == "18:46:00" || $DATE1 == "18:46:00" ]]
     then
 #        conda activate mytrader
         rm -rf alert_5min.log
-#        rm -rf alert_5min_1.log
-#        rm -rf alert_5min_2.log
-
         nohup python3 binance_websocket.py > alert_5min.log 2>&1 &
-#        nohup python3 binance_websocket.py 1 > alert_5min_1.log 2>&1 &
-#        nohup python3 binance_websocket.py 2 > alert_5min_2.log 2>&1 &
+
         echo "start"
         sleep 1s
     fi
