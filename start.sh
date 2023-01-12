e5d0c9dbb7ffb88090527d3045937d423e716651
Jack <jiazhenz@umich.edu>
2023-01-11 21:30:54 -0500

    price/volume alert

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ do
     DATE=`date | cut -d' ' -f4`
     DATE1=`date | cut -d' ' -f5`
 
-    if [[ $DATE == "20:43:00" || $DATE1 == "20:43:00" ]]
+    if [[ $DATE == "02:39:00" || $DATE1 == "02:39:00" ]]
     then
 #        conda activate mytrader
         rm -rf alert_5min.log
